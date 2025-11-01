159152383885039bd590eaefefd0039363be37bf
Calangiu <calangiualexandra22@stud.ase.ro>
2025-11-02 00:17:36 +0200

    Adaugare atribute de multiple tipuri

--- AnimaleCompanie/AnimaleCompanie/main.cpp
+++ AnimaleCompanie/AnimaleCompanie/main.cpp
@@ -1,68 +1,106 @@
 #include <iostream>
 #include <string>
-
+#include <cstring>
 using namespace std;
 
 class Caine {
-private:
-    string rasa;
-    int varsta;
-    double greutate;
-
 public:
-    Caine(string r, int v, double g) : rasa(r), varsta(v), greutate(g) {}
+    string rasa;           // atribut normal
+    int varsta;            // atribut normal
+    double greutate;       // atribut normal
+    static int nrCaini;    // atribut static
+    const string tip;      // atribut constant
+    char* sunet;           // pointer alocat dinamic (in heap)
+
+    Caine(string r, int v, double g)
+        : rasa(r), varsta(v), greutate(g), tip("Caine") {
+        nrCaini++;
+        sunet = new char[strlen("Ham! Ham!") + 1];
+        strcpy_s(sunet, strlen("Ham! Ham!") + 1, "Ham! Ham!");
+    }
 
     void latra() {
-        cout << "Ham! Ham!" << endl;
+        cout << sunet << endl;
     }
 
     void afiseaza() {
-        cout << "Caine de rasa " << rasa
+        cout << tip << " de rasa " << rasa
             << ", varsta: " << varsta
             << " ani, greutate: " << greutate << " kg." << endl;
     }
+
+    ~Caine() {
+        delete[] sunet;
+    }
 };
 
+int Caine::nrCaini = 0;
+
 class Pisica {
-private:
+public:
     string nume;
     string culoare;
     bool esteSterilizata;
+    static int nrPisici;
+    const string tip;
+    char* sunet;
 
-public:
-    Pisica(string n, string c, bool s) : nume(n), culoare(c), esteSterilizata(s) {}
+    Pisica(string n, string c, bool s)
+        : nume(n), culoare(c), esteSterilizata(s), tip("Pisica") {
+        nrPisici++;
+        sunet = new char[strlen("Miau!") + 1];
+        strcpy_s(sunet, strlen("Miau!") + 1, "Miau!");
+    }
 
     void toarce() {
-        cout << nume << " toarce linistit." << endl;
+        cout << nume << " toarce linistit. " << sunet << endl;
     }
 
     void afiseaza() {
-        cout << "Pisica " << nume
+        cout << tip << " " << nume
             << ", culoare: " << culoare
             << ", " << (esteSterilizata ? "sterilizata" : "nesterilizata") << "." << endl;
     }
+
+    ~Pisica() {
+        delete[] sunet;
+    }
 };
 
+int Pisica::nrPisici = 0;
 
 class Papagal {
-private:
+public:
     string specie;
     string culoare;
     bool vorbeste;
+    static int nrPapagali;
+    const string tip;
+    char* sunet;
 
-public:
-    Papagal(string s, string c, bool v) : specie(s), culoare(c), vorbeste(v) {}
+    Papagal(string s, string c, bool v)
+        : specie(s), culoare(c), vorbeste(v), tip("Papagal") {
+        nrPapagali++;
+        sunet = new char[strlen("Cip-cirip!") + 1];
+        strcpy_s(sunet, strlen("Cip-cirip!") + 1, "Cip-cirip!");
+    }
 
     void canta() {
-        cout << "Cip-cirip! Cip-cirip!" << endl;
+        cout << sunet << endl;
     }
 
     void afiseaza() {
-        cout << "Papagal de specie " << specie
+        cout << tip << " de specie " << specie
             << ", culoare: " << culoare
             << ", " << (vorbeste ? "vorbeste" : "nu vorbeste") << "." << endl;
     }
+
+    ~Papagal() {
+        delete[] sunet;
+    }
 };
+
+int Papagal::nrPapagali = 0;
 
 int main() {
     Caine rex("Labrador", 3, 25.5);
@@ -78,6 +116,9 @@ int main() {
     coco.afiseaza();
     coco.canta();
 
+    cout << "\nNumar total de caini: " << Caine::nrCaini << endl;
+    cout << "Numar total de pisici: " << Pisica::nrPisici << endl;
+    cout << "Numar total de papagali: " << Papagal::nrPapagali << endl;
+
     return 0;
 }
-
